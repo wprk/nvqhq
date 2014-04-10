7a274667243b242b957d2618f81fad66e1c2622f
Will Parker <wprk14@gmail.com>
2014-04-10 11:45:34 +0000

    WIP

--- client/views/courses/course-list.js
+++ client/views/courses/course-list.js
@@ -1,30 +1,5 @@
-var courseData = [
-  {
-    title: 'Hairdressing',
-    type: 'NVQ',
-    level: '1'
-  },
-  {
-    title: 'Barbering',
-    type: 'NVQ',
-    level: '1'
-  },
-  {
-    title: 'Hairdressing',
-    type: 'NVQ',
-    level: '2'
-  },
-  {
-    title: 'Barbering',
-    type: 'NVQ',
-    level: '2'
-  },
-  {
-    title: 'Hairdressing',
-    type: 'NVQ',
-    level: '3'
-  }
-];
 Template.listCourses.helpers({
-  courses: courseData
+  courses: function() {
+    return Courses.find();
+  }
 });
