63adc5c9607a8dfb3c4d1ffcfa967a68287b6b0a
Will Parker <will@jobs.ac.uk>
2014-04-10 12:19:11 +0100

    WIP

---
+++ client/views/courses/course-list.js
@@ -0,0 +1,30 @@
+var courseData = [
+  {
+    title: 'Hairdressing'
+    type: 'NVQ',
+    level: '1'
+  },
+  {
+    title: 'Barbering'
+    type: 'NVQ',
+    level: '1'
+  },
+  {
+    title: 'Hairdressing'
+    type: 'NVQ',
+    level: '2'
+  },
+  {
+    title: 'Barbering'
+    type: 'NVQ',
+    level: '2'
+  },
+  {
+    title: 'Hairdressing'
+    type: 'NVQ',
+    level: '3'
+  }
+];
+Template.listCourses.helpers({
+  courses: courseData
+});
