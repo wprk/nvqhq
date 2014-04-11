61088d5b966d765fa285bc2da3fbe3ba94c8563a
Will Parker <will@jobs.ac.uk>
2014-04-11 13:48:23 +0100

    WIP

--- client/views/application/auth/login.js
+++ client/views/application/auth/login.js
@@ -1,5 +1,24 @@
-// Template.user_loggedout.events({
-//   "click #loginWithFacebook": function (event, form) {
-//     Meteor.loginWithFacebook();
-//   }
-// });
+Template.accountLoggedOut.events({
+  "click #loginWithFacebook": function (event, form) {
+    Meteor.loginWithFacebook();
+  }
+});
+
+Template.accountLoggedOut.events({
+  "click #loginWithTwitter": function (event, form) {
+    Meteor.loginWithTwitter();
+  }
+});
+
+Template.accountLoggedOut.events({
+  "click #loginWithGoogle": function (event, form) {
+    Meteor.loginWithGoogle();
+  }
+});
+
+Template.accountLoggedOut.events({
+  "click #login": function (event, form) {
+    alert('coming soon');
+    //Meteor.login();
+  }
+});
