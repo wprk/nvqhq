08408df134e68f86aaf544b8325cb074c7cbdf4c
Will Parker <wprk14@gmail.com>
2014-04-12 21:21:22 +0000

    WIP

--- server/publications.js
+++ server/publications.js
@@ -1,4 +1,12 @@
-Meteor.publish('courses', function() {
+Meteor.publish('allCourses', function() {
+  return Courses.find({});
+});
+
+Meteor.publish('admCourses', function() {
+  return Courses.find({status: true});
+});
+
+Meteor.publish('lnrCourses', function() {
   return Courses.find({status: true});
 });
 
