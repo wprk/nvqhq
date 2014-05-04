ccd9876cb9a3dc751bf2d28a0e6a4eef48d5619b
Will Parker <wprk14@gmail.com>
2014-05-04 16:47:08 +0100

    WIP

--- collections/courses.js
+++ collections/courses.js
@@ -9,12 +9,12 @@ if (Meteor.isClient) {
   UI.registerHelper('organisationCourses', function() {
       return OrganisationCourses.find();
   });
-  UI.registerHelper('userCoursesData', function() {
+  UI.registerHelper('userCourseData', function() {
   	var courseContent = this;
 	var courses = Courses.find({'_id': courseContent.course_id});
     return _.extend(courseContent, _.omit(courses, '_id'));
   });
-  UI.registerHelper('organisationCoursesData', function() {
+  UI.registerHelper('organisationCourseData', function() {
   	var courseContent = this;
 	var courses = Courses.find({'_id': courseContent.organisation_id});
     return _.extend(courseContent, _.omit(courses, '_id'));
