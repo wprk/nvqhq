f456267ad7c942e1b80aa7cb8ec574cf406c6a16
Will Parker <Will.Parker@widerplan.com>
2014-06-27 08:44:27 +0100

    Converted to tabs from spaces

--- client/views/application/auth/registration/organisation-registration.js
+++ client/views/application/auth/registration/organisation-registration.js
@@ -1,31 +1,31 @@
 Template.organisationRegistration.events({
-  "click #register": function (event) {
-    event.preventDefault();
+	"click #register": function (event) {
+		event.preventDefault();
 
-    var firstName = $('input#firstName').val(),
-    familyName = $('input#familyName').val(),
-    username = firstName + '.' + familyName,
-    email = $('input#email').val(),
-    password = $('input#password').val(),
-    profile = {
-    	name: firstName + ' ' + familyName,
-        organisation: {
-            _id: "NOT_YET_SET",
-            verified: 0,
-            verified_by: 0
-        }
-    };
+		var firstName = $('input#firstName').val(),
+		familyName = $('input#familyName').val(),
+		username = firstName + '.' + familyName,
+		email = $('input#email').val(),
+		password = $('input#password').val(),
+		profile = {
+			name: firstName + ' ' + familyName,
+			organisation: {
+				_id: "NOT_YET_SET",
+				verified: 0,
+				verified_by: 0
+			}
+		};
 
-    Accounts.createUser({
-    	email: email,
-    	username: username,
-    	password: password,
-    	profile: profile,
-        roles: ["superadmin"]
-    }, function(error) {
-        if (error) {
-            Errors.throw('danger', error);
-        }
-    });
-  }
+		Accounts.createUser({
+			email: email,
+			username: username,
+			password: password,
+			profile: profile,
+			roles: ["superadmin"]
+		}, function(error) {
+			if (error) {
+				Errors.throw('danger', error);
+			}
+		});
+	}
 });
